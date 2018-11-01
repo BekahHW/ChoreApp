60746ede5cf960755d8a0de0c31194b78c27e0d1
BekahHW <rhawrot@gmail.com>
2018-11-01 18:32:21 -0400

    add login route to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,4 +28,16 @@ class ApplicationController < Sinatra::Base
     end
   end
 
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/success'
+    else
+      redirect '/failure'
+    end
+  end
+
+  
 end
