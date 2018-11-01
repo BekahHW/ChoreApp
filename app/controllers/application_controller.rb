a757392c6aa1b4e157e451238fb2662ca8fc47b3
BekahHW <rhawrot@gmail.com>
2018-11-01 18:37:21 -0400

    Add helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -56,4 +56,14 @@ class ApplicationController < Sinatra::Base
     redirect '/'
   end
 
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
+
 end
