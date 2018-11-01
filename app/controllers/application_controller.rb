c0df9ce9ade32ecb6a6219bbaa18d1480c3d2723
BekahHW <rhawrot@gmail.com>
2018-11-01 18:28:15 -0400

    Enable sessions and create signup route for application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,31 @@
 require './config/environment'
+require './app/models/user'
 
 class ApplicationController < Sinatra::Base
 
   configure do
-    set :public_folder, 'public'
+    # set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, 'password_security'
   end
 
-  get "/" do
+  get '/' do
     erb :welcome
+  end
+
+  get '/signup' do
+    erb :signup
+  end
+
+  post '/signup' do
+    @user = User.new(:username => params[:username], :password => params[:password])
+
+    if @user.save
+      redirect '/login'
+    else
+      redirect '/failure'
+    end
   end
 
 end
