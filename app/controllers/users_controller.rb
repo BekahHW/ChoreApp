bfbe9e8c74d22b0fb860d5ab58c1f93a4f9be6cd
BekahHW <rhawrot@gmail.com>
2018-11-02 13:12:41 -0400

    Move login, logout, signup routes from app_controller to users_cont

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,45 @@ class UsersController < ApplicationController
     erb :index
   end
 
+  get '/signup' do
+    erb :signup
+  end
+
+  post '/signup' do
+    @user = User.new(:username => params[:username], :password => params[:password])
+
+    if @user.save
+      redirect '/login'
+    else
+      redirect '/failure'
+    end
+  end
+    post '/login' do
+    @user = User.find_by(:username => params[:username])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/success'
+    else
+      redirect '/failure'
+    end
+  end
+
+  get '/success' do
+    if logged_in?
+      erb :success
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/failure' do
+    erb :failure
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/'
+  end
+
 end
