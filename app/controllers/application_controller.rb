7de7ceb7766a246d017ae601ecc50715645e66a0
BekahHW <rhawrot@gmail.com>
2018-11-02 13:42:35 -0400

    set public folder to public in AppCon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
-    # set :public_folder, 'public'
+    set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
     set :session_secret, 'password_security'
