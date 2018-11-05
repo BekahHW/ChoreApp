32e5882e23598a4825ea524a2898a2e63156c6a7
BekahHW <rhawrot@gmail.com>
2018-11-05 13:46:57 -0500

    Change password to password_digest in users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 5) do
 
   create_table "chores", force: :cascade do |t|
     t.string "type"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "email"
+    t.string "password_digest"
   end
 
 end
