711bf3d898b2e71b3a3b6527fd83726905c4d385
BekahHW <rhawrot@gmail.com>
2018-11-06 13:56:35 -0500

    Create new family_member route

--- app/controllers/family_member_controller.rb
+++ app/controllers/family_member_controller.rb
@@ -1,3 +1,12 @@
 class FamilyMemberController < ApplicationController
+  get '/family_members/new' do
+    @family_member = FamilyMember.all
+    erb :'/family_member/new'
+  end
+
+  post '/family_members/new' do
+    @family_member = FamilyMember.new(:name => params[:name])
+    erb :'/family_member/show'
+  end
 
 end
