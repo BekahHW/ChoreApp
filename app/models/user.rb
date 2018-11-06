b442ee032ea0db7462c92315dd3618b071be1db1
BekahHW <rhawrot@gmail.com>
2018-11-06 13:58:17 -0500

    Create slug methods in User and route in controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :family_members
+
+  def slug
+    username.strip.downcase.gsub /\W+/, '-'
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find{|a| a.slug == slug}
+  end
+
 end
