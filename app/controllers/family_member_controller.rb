ae276919a2173611a7527adc3d0ac011bb76c407
BekahHW <rhawrot@gmail.com>
2018-11-06 14:16:12 -0500

    Create edit and show route, and show page

--- app/controllers/family_member_controller.rb
+++ app/controllers/family_member_controller.rb
@@ -9,4 +9,23 @@ class FamilyMemberController < ApplicationController
     erb :'/family_member/show'
   end
 
+  get '/family_member/edit' do
+    @family_member = FamilyMember.find(params[:slug])
+    erb :'family_member/edit'
+  end
+
+  patch '/family_member/:id' do
+    @family_member = FamilyMember.find(params[:slug])
+    erb :'/family_member/show'
+  end
+
+  delete '/family_member/:id/delete' do
+    if logged_in?
+      @family_member = FamilyMember.find(params[:slug])
+      @family_member.destroy
+      redirect '/family_member'
+    else
+      erb :'users/login'
+    end
+  end
 end
