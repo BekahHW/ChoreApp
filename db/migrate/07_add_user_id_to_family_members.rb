05dc5e28a316fbf0bdb5ef3af11216c37ba5a72f
BekahHW <rhawrot@gmail.com>
2018-11-08 13:54:39 -0500

    Pluralize family_members in add user_id

--- db/migrate/07_add_user_id_to_family_members.rb
+++ db/migrate/07_add_user_id_to_family_members.rb
@@ -1,5 +1,5 @@
 class AddUserIdToFamilyMembers< ActiveRecord::Migration
   def change
-    add_column :family_member, :user_id, :integer
+    add_column :family_members, :user_id, :integer
   end
 end
