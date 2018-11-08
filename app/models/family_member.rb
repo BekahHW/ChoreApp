14be06b5d259457f5a700715d9fa5636408b1804
BekahHW <rhawrot@gmail.com>
2018-11-08 05:30:15 -0500

    Add delete form to family_member show

--- app/models/family_member.rb
+++ app/models/family_member.rb
@@ -3,7 +3,7 @@ class FamilyMember < ActiveRecord::Base
   belongs_to :user
 
   def slug
-    username.strip.downcase.gsub /\W+/, '-'
+    name.strip.downcase.gsub /\W+/, '-'
   end
 
   def self.find_by_slug(slug)
