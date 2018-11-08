a13d5d1f0d6d00fa08db9453f52322027db59538
BekahHW <rhawrot@gmail.com>
2018-11-08 14:23:33 -0500

    Add show.erb for chores

--- app/controllers/chore_controller.rb
+++ app/controllers/chore_controller.rb
@@ -7,23 +7,24 @@ class ChoreController < ApplicationController
   end
 
   post '/chore' do
-    binding.pry
     @chore = Chore.create(params[:chore])
 
-    if !params['family_member']['name'].empty?
-      @chore.family_member << Chore.create(:description => params['chore']['description'], :room =>['chore']['room'], :day =>['chore']['day'])
-    end
+    # if !params['family_member']['name'].empty?
+    #   @chore.family_member << Chore.create(:description => params['chore']['description'], :room =>['chore']['room'], :day =>['chore']['day'])
+    # end
     @chore.save
+    binding.pry
     redirect "chore/#{@chore.id}"
   end
 
 
   patch '/chore/:id' do
+
     @chore = Chore.find_by_id(params[:id])
     @chore.description = params[:description]
     @chore.room = params[:room]
     @chore.day = params[:day]
     @chore.save
-    erb :show
+    erb :'/chore/show'
   end
 end
