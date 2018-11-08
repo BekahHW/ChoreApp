029f52f412177c20556cf28e2a657d0af55d70fd
BekahHW <rhawrot@gmail.com>
2018-11-08 09:15:10 -0500

    Add post chore route

--- app/controllers/chore_controller.rb
+++ app/controllers/chore_controller.rb
@@ -5,4 +5,13 @@ class ChoreController < ApplicationController
     erb :'/chore/new'
   end
 
+  post '/chore' do
+    @chore = FamilyMember.create(params[:chore])
+    if !params['family_member']['name'].empty?
+      @chore.family_member << Chore.create(name: params['chore']['type'])
+    end
+    @chore.save
+    redirect "chore/#{@chore.id}"
+  end
+
 end
