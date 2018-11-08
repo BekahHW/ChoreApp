975b722b610cf8ba758796d0e4eec32f7ef54f24
BekahHW <rhawrot@gmail.com>
2018-11-08 09:27:52 -0500

    Remove type from chores table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 5) do
+ActiveRecord::Schema.define(version: 6) do
 
   create_table "chores", force: :cascade do |t|
-    t.string "type"
     t.string "description"
     t.string "room"
     t.string "day"
