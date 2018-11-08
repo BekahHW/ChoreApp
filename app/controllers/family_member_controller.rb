21cf057b0f0d9f8f37db589a37da29ac328ba57d
BekahHW <rhawrot@gmail.com>
2018-11-08 08:57:31 -0500

    add chores to familymember/new route

--- app/controllers/family_member_controller.rb
+++ app/controllers/family_member_controller.rb
@@ -6,8 +6,15 @@ class FamilyMemberController < ApplicationController
 
   post '/family_member/new' do
     @family_member = FamilyMember.create(:name => params[:name])
-    erb :'/family_member/show'
+    if !params['chore']['type'].empty?
+      @family_member.chore = Chore.create(:type => params['chore']['type'])
+    end
+    @pet.save
+    redirect to "pets/#{@pet.id}"
   end
+
+
+
 
   get '/family_member/edit' do
     @family_member = FamilyMember.find_by_slug(params[:slug])
