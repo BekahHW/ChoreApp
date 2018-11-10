3541357244164e83be727fae9bac28bb26e5addb
BekahHW <rhawrot@gmail.com>
2018-11-09 21:18:25 -0500

    Add family_member/:slug/edit

--- app/controllers/family_member_controller.rb
+++ app/controllers/family_member_controller.rb
@@ -5,16 +5,6 @@ class FamilyMemberController < ApplicationController
     erb :'/family_member/new'
   end
 
-  post '/family_member/new' do
-    @family_member = FamilyMember.find_or_create_by(:name => params[:name])
-    # if !params['chore']['description'].empty?
-
-    # @family_member.chore = Chore.create(params['chore'])
-    # end
-    @family_member.save
-    redirect("/family_member/#{@family_member.slug}")
-  end
-
   get '/family_member/:slug' do
     @family_member = FamilyMember.find_by_slug(params[:slug])
     erb :'/family_member/show'
@@ -25,6 +15,16 @@ class FamilyMemberController < ApplicationController
     erb :'family_member/edit'
   end
 
+  post '/family_member/new' do
+    @family_member = FamilyMember.find_or_create_by(:name => params[:name])
+    # if !params['chore']['description'].empty?
+
+    # @family_member.chore = Chore.create(params['chore'])
+    # end
+    @family_member.save
+    redirect("/family_member/#{@family_member.slug}")
+  end
+
   patch '/family_member/:slug' do
     @family_member = FamilyMember.find_by_slug(params[:slug])
     @family_member.update(params[:family_member])
@@ -32,7 +32,7 @@ class FamilyMemberController < ApplicationController
     erb :'/family_member/show'
   end
 
-  delete '/family_member/:slug/delete' do
+  delete '/family_member/:slug' do
     if logged_in?
       @family_member = FamilyMember.find_by_slug(params[:slug])
       @family_member.destroy
