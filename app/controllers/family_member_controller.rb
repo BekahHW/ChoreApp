3d744ffcadfd7010d4b2a4cf8af0cb6ee558b10c
BekahHW <rhawrot@gmail.com>
2018-11-10 09:26:18 -0500

    Reorder family_member controller

--- app/controllers/family_member_controller.rb
+++ app/controllers/family_member_controller.rb
@@ -5,21 +5,6 @@ class FamilyMemberController < ApplicationController
     erb :'/family_member/new'
   end
 
-  post '/family_member' do
-    @family_member = FamilyMember.create(params[:family_member])
-    # if !params['chore']['description'].empty?
-
-    # @family_member.chore = Chore.create(params['chore'])
-    # end
-    # @family_member.save
-    redirect("/family_member/#{@family_member.slug}")
-  end
-
-  get '/family_member/:slug' do
-    @family_member = FamilyMember.find_by_slug(params[:slug])
-    erb :'/family_member/show'
-  end
-
   get '/family_member/:slug/edit' do
     @family_member = FamilyMember.find_by_slug(params[:slug])
     erb :'family_member/edit'
@@ -29,7 +14,21 @@ class FamilyMemberController < ApplicationController
     @family_member = FamilyMember.find_by_slug(params[:slug])
     @family_member.update(params[:family_member])
     @family_member.save
+    redirect "/family_member/#{@family_member.slug}"
+  end
+
+  get '/family_member/:slug' do
+    @family_member = FamilyMember.find_by_slug(params[:slug])
     erb :'/family_member/show'
+  end
+
+  post '/family_member' do
+    @family_member = FamilyMember.create(params[:family_member])
+    if !params['chore']['description'].empty? || !params['chore']['room'].empty? || !params['chore']['day'].empty?
+      @family_member.chore << Chore.create(params['chore'])
+    end
+    @family_member.save
+    redirect("/family_member/#{@family_member.slug}")
   end
 
   delete '/family_member/:slug' do
