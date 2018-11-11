53e5de8829b638e006ee004629c558b21e18241d
BekahHW <rhawrot@gmail.com>
2018-11-10 19:14:18 -0500

    change family_member slug to id

--- app/controllers/family_member_controller.rb
+++ app/controllers/family_member_controller.rb
@@ -10,20 +10,26 @@ class FamilyMemberController < ApplicationController
     erb :'/family_member/new'
   end
 
-  get '/family_member/:slug/edit' do
-    @family_member = FamilyMember.find_by_slug(params[:slug])
+  get '/family_member/:id/edit' do
+    @family_member = FamilyMember.find(params[:id])
     erb :'family_member/edit'
   end
 
-  patch '/family_member/:slug' do
-    @family_member = FamilyMember.find_by_slug(params[:slug])
-    # @family_member.update(params[:family_member])
-    @family_member.save
-    redirect "/family_member/#{@family_member.slug}"
+  post '/family_member/:id' do
+    @family_member = FamilyMember.find(params[:id])
+    @family_member.update(params.select {|f| f == 'name'})
+    redirect "/family_member/#{@family_member.id}"
   end
 
-  get '/family_member/:slug' do
-    @family_member = FamilyMember.find_by_slug(params[:slug])
+  patch '/family_member/:id' do
+    @family_member = FamilyMember.find_by_id(params[:id])
+    # @family_member.update(params[:family_member])
+    @family_member.save
+    redirect "/family_member/#{@family_member.id}"
+  end
+
+  get '/family_member/:id' do
+    @family_member = FamilyMember.find(params[:id])
     erb :'/family_member/show'
   end
 
@@ -33,12 +39,12 @@ class FamilyMemberController < ApplicationController
     #   @family_member.chore << Chore.create(params['chore'])
     # end
     # @family_member.save
-    redirect("/family_member/#{@family_member.slug}")
+    redirect("/family_member/#{@family_member.id}")
   end
 
-  delete '/family_member/:slug' do
+  delete '/family_member/:id' do
     if logged_in?
-      @family_member = FamilyMember.find_by_slug(params[:slug])
+      @family_member = FamilyMember.find_by_id(params[:id])
       # @family_member = current_user.FamilyMember.find_by_slug(params[:slug])
       @family_member.destroy
       redirect to '/family_member/new'
