3e76977247d16a09858d3a234e77cc6211281388
BekahHW <rhawrot@gmail.com>
2018-11-10 20:38:24 -0500

    Reorder chore routes, change chore route to get in family_mem/show

--- app/controllers/chore_controller.rb
+++ app/controllers/chore_controller.rb
@@ -1,9 +1,43 @@
 require 'pry'
 class ChoreController < ApplicationController
+  get '/chore' do
+    @chore = Chore.all
+    erb :'chore/index'
+  end
 
   get '/chore/new' do
-    @chore = Chore.all
+    # @chore = Chore.all
     erb :'/chore/new'
+  end
+
+  get '/chore/:id/edit' do
+    if logged_in?
+      @chore = Chore.find_by_id(params[:id])
+      erb :'chore/edit'
+    end
+  end
+
+  post '/chore/:id' do
+    @chore = Chore.find(params[:id])
+    # @chore.update(params['chore'])
+    # if !params['family_member']['name'].empty?
+    #   @chore.family_member << FamilyMember.create(name: params['family_member']['name'])
+    # end
+    # @chore.save
+    redirect "chore/#{@chore.id}"
+  end
+
+  get '/chore/:id' do
+    if logged_in?
+      @chore = Chore.find_by_id(params[:id])
+      # @chore.description = params[:description]
+      # @chore.room = params[:room]
+      # @chore.day = params[:day]
+      # @chore.save
+      erb :'/chore/show'
+    else
+      redirect '/chore'
+    end
   end
 
   post '/chore' do
@@ -21,36 +55,5 @@ class ChoreController < ApplicationController
       redirect "chore/#{@chore.id}"
     end
   end
-
-  get '/chore/:id/edit' do
-    if logged_in?
-      @chore = Chore.find_by_id(params[:id])
-      erb :'chore/edit'
-    end
-  end
-
-  get '/chore/:id' do
-    if logged_in?
-      @chore = Chore.find_by_id(params[:id])
-      # @chore.description = params[:description]
-      # @chore.room = params[:room]
-      # @chore.day = params[:day]
-      # @chore.save
-      erb :'/chore/show'
-    else
-      redirect '/chore'
-    end
-  end
-
-  post '/chore/:id' do
-    @chore = Chore.find_by_id(params[:id])
-    @chore.update(params['chore'])
-    if !params['family_member']['name'].empty?
-      @chore.family_member << FamilyMember.create(name: params['family_member']['name'])
-    end
-    @chore.save
-    redirect "chore/#{@chore.id}"
-  end
-
 
 end
