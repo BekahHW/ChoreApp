f4c9a75bf7c4e5d89ca8b36235b28c43ee9178ef
BekahHW <rhawrot@gmail.com>
2018-11-12 06:12:06 -0500

    Fix edit route for chore

--- app/controllers/chore_controller.rb
+++ app/controllers/chore_controller.rb
@@ -6,33 +6,30 @@ class ChoreController < ApplicationController
   end
 
   get '/chore/new' do
-    # @chore.family_member_id = current_user.id
-
     @chore = Chore.all
     erb :'/chore/new'
   end
 
   get '/chore/:id/edit' do
     if logged_in?
-      @chore = Chore.find_by_id(params[:id])
+      @chore = Chore.find(params[:id])
       erb :'chore/edit'
     end
   end
 
   post '/chore/:id' do
     @chore = Chore.find(params[:id])
-    # @chore.update(params['chore'])
+    @chore.update(params['chore'])
     # if !params['family_member']['name'].empty?
     #   @chore.family_member << FamilyMember.create(name: params['family_member']['name'])
     # end
-    # @chore.save
+    @chore.save
     redirect "chore/#{@chore.id}"
   end
 
   patch '/chore/:id' do
     @chore = Chore.find(params[:id])
     # @family_member.update(params[:family_member])
-    @chore.save
     redirect "chore/#{@chore.id}"
   end
 
