b1f36c9ea672a8a36c3c9ccc1ba0fb5ad37f23db
BekahHW <rhawrot@gmail.com>
2018-11-14 05:38:33 -0500

    Change name os sessions secret

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, 'password_security'
+    set :session_secret, 'family_member_security'
   end
 
   get '/' do
