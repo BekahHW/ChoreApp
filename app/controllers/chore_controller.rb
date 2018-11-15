63836e828c1122e2625f6cad7583e9a8ee0d9927
BekahHW <rhawrot@gmail.com>
2018-11-15 10:42:33 -0500

    Pluralize chore in controller

--- app/controllers/chore_controller.rb
+++ app/controllers/chore_controller.rb
@@ -1,65 +1,63 @@
 require 'pry'
 class ChoreController < ApplicationController
-  get '/chore' do
+  get '/chores' do
     @chore = Chore.all
     erb :'chore/index'
   end
 
-  get '/chore/new' do
+  get '/chores/new' do
     if logged_in?
-      # @chore = Chore.all
-      # @family_member = FamilyMember.all
+      @family_member = current_user.family_members.all
       erb :'/chore/new'
     else
       redirect to '/'
     end
   end
 
-  get '/chore/:id/edit' do
+  get '/chores/:id/edit' do
     if logged_in?
       @chore = Chore.find(params[:id])
       erb :'chore/edit'
     end
   end
 
-  post '/chore/:id' do
+
+
+
+  # post '/chore/:id' do
+  #   @chore = Chore.find(params[:id])
+  #   redirect "chore/#{@chore.id}"
+  # end
+
+  patch '/chores/:id' do
     @chore = Chore.find(params[:id])
-    # @chore.update(params['chore'])
-    # @chore.save
+
     redirect "chore/#{@chore.id}"
   end
 
-  patch '/chore/:id' do
-    @chore = Chore.find(params[:id])
-    redirect "chore/#{@chore.id}"
-  end
-
-  get '/chore/:id' do
+  get '/chores/:id' do
     if logged_in?
       @chore = Chore.find(params[:id])
+
       erb :'/chore/show'
     else
       redirect '/chore'
     end
   end
 
-  post '/chore' do
+  post '/chores' do
     if logged_in?
       if params[:chore] == ""
         redirect '/chore/new'
       end
-      # if !params['family_member_id'].empty?
-
-      @family_member = current_user.family_members
-      # FamilyMember.create(name: params['family_member']['name'])
-      # end
-      @chore = Chore.find_or_create_by(params[:chore])
-      # @family_member = FamilyMember.all
+      @family_member = FamilyMember.create(params['family_member'])
+      # @family_member.save
+      @chore = Chore.create(params[:chore])
       redirect "chore/#{@chore.id}"
     end
   end
 
-  delete '/chore/:id' do
+  delete '/chores/:id' do
     if logged_in?
       @chore= Chore.find(params[:id])
       @chore.destroy
