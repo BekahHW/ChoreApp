b283c82ee9c16cad35768e829f90adb156a1fdad
BekahHW <rhawrot@gmail.com>
2018-11-17 05:47:56 -0500

    Clean up commented code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
 
     def redirect_if_not_logged_in
       if !logged_in?
-        redirect "/users/failure?error=Please log in first"
+        redirect "/users/login?error=Please log in first"
       end
     end
 
@@ -29,18 +29,6 @@ class ApplicationController < Sinatra::Base
     def current_user
       User.find(session[:user_id])
     end
-
-    # def login(user_id, password)
-    #   session[:user_id] = user.id
-    # end
-    #
-    # def logout!
-    #   s ession.clear
-    # end
-
-    #    def current_user
-    #   @current_user ||= User.find_by_id(session[:user_id])
-    # end
   end
 
 end
