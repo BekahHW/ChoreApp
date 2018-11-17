f4e15267bc1f4023e7ae9359477ab11560164d76
BekahHW <rhawrot@gmail.com>
2018-11-17 06:14:42 -0500

    Del find by slug methods for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,4 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :family_members
-
-  def slug
-    username.strip.downcase.gsub /\W+/, '-'
-  end
-
-  def self.find_by_slug(slug)
-    self.all.find{|a| a.slug == slug}
-  end
-
 end
